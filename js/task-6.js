6173c0f254c9339843db40ae98b8fa82cc3bbfd1
OlikKorsun <145294542+OlikKorsun@users.noreply.github.com>
2024-01-28 00:13:54 +0200

    dz7
    task6 - done js+css

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,42 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const ourInput = document.querySelector("#controls").querySelector("input");
+const buttonCreate = document.querySelector("button[data-create]");
+const buttonDestroy = document.querySelector("button[data-destroy]");
+const divWithBoxes = document.querySelector("#boxes");
+
+function createDiv() {
+  const amount = parseInt(ourInput.value);
+  if ((amount >= 1) && (amount <= 100)) {
+    createBoxes(amount);
+  }
+    ourInput.value = "";
+}
+
+function createBoxes(amount) {
+  if (divWithBoxes.hasChildNodes) {
+    destroyBoxes()
+  }
+  for (let i = 0; i < amount; i++) {
+    const ourDiv = document.createElement("div");
+    ourDiv.style.width = `${30 + (i * 10)}px`;
+    ourDiv.style.height = `${30 + (i * 10)}px`;
+    ourDiv.style.backgroundColor = getRandomHexColor();
+    divWithBoxes.appendChild(ourDiv);
+  }
+}
+
+function destroyBoxes() {
+  // if (divWithBoxes.hasChildNodes) {
+  //   divWithBoxes.removeChild(divWithBoxes.firstChild);
+  // } 
+  // прикольно получилось видаляє поелементно - можна як зверху так і знизу
+  while (divWithBoxes.firstChild) {
+      divWithBoxes.removeChild(divWithBoxes.lastChild);
+  }
+}
+
+buttonCreate.addEventListener("click", createDiv);
+buttonDestroy.addEventListener("click", destroyBoxes);
