b56983859e1be4dcc5574f263382660d41c6f050
OlikKorsun <145294542+OlikKorsun@users.noreply.github.com>
2024-01-29 22:32:46 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,7 @@ const ourForm = document.querySelector(".login-form");
 function fillForm(event) {
     event.preventDefault();
     const userEmail = event.target.elements.email.value.trim();
-    const userPassword = event.target.elements.email.value.trim();
+    const userPassword = event.target.elements.password.value.trim();
     if (userEmail === "" ||
         userPassword === "") {
         return alert("All form fields must be filled in");
